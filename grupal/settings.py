d501b0dc9f5df1f97b26e3b4b427b633cf67c5c9
ximenamar <ximenamar04@gmail.com>
2018-09-12 11:02:30 -0500

    tres

--- grupal/settings.py
+++ grupal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/principal/'
 LOGOUT_REDIRECT_URL = 'login'
+
+django_heroku.settings(locals())
