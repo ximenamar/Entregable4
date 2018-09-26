7beaca69318d8125eb84693b1d0ebcf21bf5a8ed
A2203 <A2203@lab.aca>
2018-09-26 08:26:51 -0500

    v2.0

--- main/models.py
+++ main/models.py
@@ -8,13 +8,71 @@ class Carrera(models.Model):
 
 
 class Curso(models.Model):
-    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
     nombre_curso = models.CharField(max_length=200)
     def __str__(self):
         return self.nombre_curso
 
+class Facultad(models.Model):
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
+    nombre_facultad = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_facultad
+
+class Semestre(models.Model):
+    nombre_semestre = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_semestre
 
 class Profesor(models.Model):
     nombre_profesor = models.CharField(max_length=200)
     def __str__(self):
         return self.nombre_profesor
+
+class Seccion(models.Model):
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    numero_seccion = models.CharField(max_length=200)
+    def __str__(self):
+        return self.numero_seccion
+
+class Horario(object):
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    codigo_horario = models.CharField(max_length=200)
+    inicio = models.CharField(max_length=200)
+    fin = models.CharField(max_length=200)
+    def __str__(self):
+        return self.codigo_horario
+
+class Etiqueta(models.Model):
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    asesoria = models.ForeignKey(Asesoria, on_delete=models.CASCADE)
+    nombre_etiqueta = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_etiqueta
+
+class Cita(models.Model):
+    asesoria_simple = models.ForeignKey(asesoria_simple, on_delete=models.CASCADE)
+    asesoria_multiple = models.ForeignKey(asesoria_multiple, on_delete=models.CASCADE)
+    codigo_cita = models.CharField(max_length=200)
+    fecha = models.CharField(max_length=200)
+    razon = models.CharField(max_length=200)
+    lugar = models.CharField(max_length=200)
+    def __str__(self):
+        return self.codigo_cita
+
+class Asesoria_simple(models.Model):
+    codigo_asesoria_simple = models.CharField(max_length=200)
+    def __str__(self):
+        return self.codigo_asesoria_simple
+
+class Asesoria_multiple(models.Model):
+    codigo_asesoria_multiple = models.CharField(max_length=200)
+    def __str__(self):
+        return self.codigo_asesoria_multiple
+
+class Alumno(models.Model):
+    nombre_alumno = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre_alumno
