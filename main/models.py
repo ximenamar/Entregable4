09091797810e7a27475fc46a56e5b49b433524f6
rodrigo arce zarate <20150087@aloe.ulima.edu.pe>
2018-10-16 13:02:43 -0500

    make it better

--- main/models.py
+++ main/models.py
@@ -2,43 +2,43 @@ from django.db import models
 
 # Create your models here.
 class Profesor(models.Model):
-    nombre_profesor = models.CharField(max_length=200, default= 'Profesor')
+    nombre_profesor = models.CharField(max_length=200, default='Profesor')
     def __str__(self):
         return self.nombre_profesor
 
 class Alumno(models.Model):
-    nombre_alumno = models.CharField(max_length=200, default= 'Alumno')
+    nombre_alumno = models.CharField(max_length=200, default='Alumno')
     def __str__(self):
         return self.nombre_alumno
 
 class Seccion(models.Model):
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
-    numero_seccion = models.CharField(max_length=200, default= 'Seccion')
+    numero_seccion = models.CharField(max_length=200, default='Seccion')
     def __str__(self):
         return self.numero_seccion
 
 class Horario(object):
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
-    codigo_horario = models.CharField(max_length=200, default= 'horario')
+    codigo_horario = models.CharField(max_length=200, default='horario')
     inicio = models.CharField(max_length=200)
     fin = models.CharField(max_length=200)
     def __str__(self):
         return self.codigo_horario
 
 class Asesoria_simple(models.Model):
-    codigo_asesoria_simple = models.CharField(max_length=200, default= 'AseSim')
+    codigo_asesoria_simple = models.CharField(max_length=200, default='AseSim')
     def __str__(self):
         return self.codigo_asesoria_simple
 
 class Asesoria_multiple(models.Model):
-    codigo_asesoria_multiple = models.CharField(max_length=200, default= 'AseMul')
+    codigo_asesoria_multiple = models.CharField(max_length=200, default='AseMul')
     def __str__(self):
         return self.codigo_asesoria_multiple
 
 class Cita(models.Model):
     asesoria_simple = models.ForeignKey(Asesoria_simple, on_delete=models.CASCADE)
     asesoria_multiple = models.ForeignKey(Asesoria_multiple, on_delete=models.CASCADE)
-    codigo_cita = models.CharField(max_length=200, default= 'Cita')
+    codigo_cita = models.CharField(max_length=200, default='Cita')
     fecha = models.CharField(max_length=200)
     razon = models.CharField(max_length=200)
     lugar = models.CharField(max_length=200)
@@ -48,31 +48,31 @@ class Cita(models.Model):
 class Etiqueta(models.Model):
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
     asesoria = models.ForeignKey(Asesoria_simple, on_delete=models.CASCADE)
-    nombre_etiqueta = models.CharField(max_length=200, default= 'Etiqueta')
+    nombre_etiqueta = models.CharField(max_length=200, default='Etiqueta')
     def __str__(self):
         return self.nombre_etiqueta
 
 class Semestre(models.Model):
-    nombre_semestre = models.CharField(max_length=200, default= 'Semestre')
+    nombre_semestre = models.CharField(max_length=200, default='Semestre')
     def __str__(self):
         return self.nombre_semestre
 
 class Carrera(models.Model):
-    nombre_carrera = models.CharField(max_length=200, default= 'Carrera')
+    nombre_carrera = models.CharField(max_length=200, default='Carrera')
     def __str__(self):
         return self.nombre_carrera
 
 class Facultad(models.Model):
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
-    nombre_facultad = models.CharField(max_length=200, default= 'Facultad')
+    nombre_facultad = models.CharField(max_length=200, default='Facultad')
     def __str__(self):
         return self.nombre_facultad
 
 class Curso(models.Model):
     facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
     seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
-    nombre_curso = models.CharField(max_length=200, default= 'Curso')
+    nombre_curso = models.CharField(max_length=200, default='Curso')
     def __str__(self):
         return self.nombre_curso
 
