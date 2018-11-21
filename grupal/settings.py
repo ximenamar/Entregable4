09e58052b167701ee692eaa3203dcefaf724b9be
rodrigo arce zarate <20150087@aloe.ulima.edu.pe>
2018-11-21 06:45:31 -0500

    FinalizadoTests

--- grupal/settings.py
+++ grupal/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'grupal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
